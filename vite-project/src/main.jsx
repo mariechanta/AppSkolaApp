bbd673c9f8cfd04db2c536e437b39569a52a8dbb
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-04-21 15:16:18 +0200

    commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {render} from "react-dom";
-import App from './components/App';
-import App from './components/App.jsx'
+import App from './components/App.jsx';
 import './index.css'
 
 
