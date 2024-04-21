06a54d7e740c6686d4e1498d8eede2b322943baa
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-04-21 14:28:32 +0200

    commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import {render} from "react-dom";
+import {render} from "react-dom";
 import App from './components/App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+ const container = document.getElementById("root");
+render(
+ <React.StrictMode>
+ <App />
+  </React.StrictMode>,
+   container
+ );
