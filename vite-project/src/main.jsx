550e453e36b39536644bc45affc5881d15d8a0e7
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-04-21 15:22:44 +0200

    commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import {render} from "react-dom";
-import App from './components/App.jsx';
-import './index.css';
+import App from "./components/App.jsx";
+import "./index.css";
 
 
  const container = document.getElementById("root");
